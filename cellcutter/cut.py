9baa115b79a0c86aa1f66f7abc5533ce96e16806
Clemens Hug <clemens.hug@gmail.com>
2021-09-01 13:22:49 -0400

    Optimize for speed at the cost of memory. Pre-padding and pre-computing mask thumbnails

--- cellcutter/cut.py
+++ cellcutter/cut.py
@@ -19,54 +19,34 @@ class Image:
 
     @property
     def n_channels(self) -> int:
-        if (len(self.base_series.shape) == 2):
+        if len(self.base_series.shape) == 2:
             return 1
         else:
             return self.base_series.shape[0]
 
 
-# Efficient way to copy crops from an image where coordinates
-# can be beyond the image boundaries
-# Avoids copying the whole image with padding
-# https://stackoverflow.com/a/55391329/4603385
-def fill_crop(img: np.ndarray, pos: int, crop: np.ndarray) -> None:
-    img_shape, pos, crop_shape = (
-        np.array(img.shape),
-        np.array(pos),
-        np.array(crop.shape),
-    )
-    end = pos + crop_shape
-    # Calculate crop slice positions
-    crop_low = np.clip(0 - pos, a_min=0, a_max=crop_shape)
-    crop_high = crop_shape - np.clip(end - img_shape, a_min=0, a_max=crop_shape)
-    crop_slices = tuple(slice(low, high) for low, high in zip(crop_low, crop_high))
-    # Calculate img slice positions
-    pos = np.clip(pos, a_min=0, a_max=img_shape)
-    end = np.clip(end, a_min=0, a_max=img_shape)
-    img_slices = tuple(slice(low, high) for low, high in zip(pos, end))
-    crop[crop_slices] = img[img_slices]
-
-
 # Processes single channel
 def cut_cells(
     img: np.ndarray,
-    segmentation_mask: np.ndarray,
     cell_data: pd.DataFrame,
     window_size: int,
-    mask_cells: bool = True,
+    mask_thumbnails: Optional[np.ndarray] = None,
     cell_stack: Optional[np.ndarray] = None,
 ) -> np.ndarray:
     if cell_stack is None:
-        cell_stack = np.zeros(
+        cell_stack = np.empty(
             (cell_data.shape[0], window_size, window_size), dtype=img.dtype
         )
+    window_size_h = window_size // 2
+    img = np.pad(img, ((window_size_h, window_size_h), (window_size_h, window_size_h)))
     for i, c in enumerate(cell_data.itertuples()):
-        centroids = np.array([c.Y_centroid, c.X_centroid]).astype(int) - window_size // 2
-        fill_crop(img, centroids, cell_stack[i, :, :])
-        if mask_cells:
-            mask = np.zeros((window_size, window_size), dtype=segmentation_mask.dtype)
-            fill_crop(segmentation_mask, centroids, mask)
-            cell_stack[i, mask != c.CellID] = 0
+        centroids = np.array([c.Y_centroid, c.X_centroid]).astype(int)
+        cell_stack[i, :, :] = img[
+            centroids[0] : centroids[0] + window_size,
+            centroids[1] : centroids[1] + window_size,
+        ]
+        if mask_thumbnails is not None:
+            cell_stack[i, :, :] *= mask_thumbnails[i]
     return cell_stack
 
 
@@ -97,15 +77,33 @@ def save_cells_all_channels(
         dtype=img.get_channel(0).dtype,
         compressor=Blosc(cname="zstd", clevel=3, shuffle=Blosc.BITSHUFFLE),
     )
+    mask_thumbnails = None
+    if mask_cells:
+        window_size_h = window_size // 2
+        segmentation_mask_img_padded = np.pad(
+            segmentation_mask_img,
+            ((window_size_h, window_size_h), (window_size_h, window_size_h)),
+        )
+        mask_thumbnails = np.empty(
+            (cell_data.shape[0], window_size, window_size), dtype=bool
+        )
+        for i, c in enumerate(cell_data.itertuples()):
+            centroids = np.array([c.Y_centroid, c.X_centroid]).astype(int)
+            mask_thumbnails[i, :, :] = (
+                segmentation_mask_img_padded[
+                    centroids[0] : centroids[0] + window_size,
+                    centroids[1] : centroids[1] + window_size,
+                ]
+                == c.CellID
+            )
     for i in range(img.n_channels):
         logging.info(f"Processing channel {i}")
         channel_img = img.get_channel(i)
         cell_stack = cut_cells(
             channel_img,
-            segmentation_mask_img,
             cell_data,
             window_size,
-            mask_cells=mask_cells,
+            mask_thumbnails=mask_thumbnails,
         )
         logging.info(f"Writing thumbnails for channel {i}")
         file[i, ...] = cell_stack
